314714a766da2cb1198fe623f622d30eaa540944
Mingfei Shao <2475897+mfshao@users.noreply.github.com>
2024-02-05 16:55:56 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'dictionaryutils',
     ],
     dependency_links=[
-       "git+https://github.com/uc-cdis/dictionaryutils.git@3.2.0#egg=dictionaryutils",
+       "git+https://github.com/uc-cdis/dictionaryutils.git@master#egg=dictionaryutils",
     ],
     package_data={
         "gdcdictionary": [
